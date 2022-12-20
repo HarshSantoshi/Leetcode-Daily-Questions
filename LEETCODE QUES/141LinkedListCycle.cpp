980758433b5e8eb9a045ee1a3076ff3edffbe6c8
Harsh Santoshi <115166906+HarshSantoshi@users.noreply.github.com>
2022-12-20 12:29:39 +0530

    Update 141LinkedListCycle.cpp

--- LEETCODE QUES/141LinkedListCycle.cpp
+++ LEETCODE QUES/141LinkedListCycle.cpp
@@ -9,17 +9,35 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL){
-            return false;
-        }    
-        ListNode*temp=head;
+    //     if(head==NULL || head->next==NULL){
+    //         return false;
+    //     }    
+    //     ListNode*temp=head;
 
-        while(temp!=NULL){
-            if(temp->val==INT_MAX){
+    //     while(temp!=NULL){
+    //         if(temp->val==INT_MAX){
+    //             return true;
+    //         }
+    //         temp->val = INT_MAX;
+    //         temp=temp->next;
+    //     }
+    //     return false;
+    // }
+
+
+    //By slow and fast method;
+         if(head==NULL || head->next==NULL){
+            return false;
+        }  
+        ListNode*slow = head;
+        ListNode*fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(fast==slow){
                 return true;
             }
-            temp->val = INT_MAX;
-            temp=temp->next;
         }
         return false;
     }
