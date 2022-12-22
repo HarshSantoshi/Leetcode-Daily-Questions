32fefa299d34f0097517f449e2da60bc3f103027
Harsh Santoshi <115166906+HarshSantoshi@users.noreply.github.com>
2022-12-22 13:40:46 +0530

    Create 834SumOfDistancesInTree.cpp

---
+++ 834SumOfDistancesInTree.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> adj;
+    vector<int> counter, res;
+        
+    void dfs(int i, int p = -1) {
+        for(auto u : adj[i]) {
+            if(u == p) continue;
+            dfs(u, i);
+            counter[i] += counter[u];
+            res[i] += res[u] + counter[u];
+        }
+        counter[i] += 1;
+    }
+
+    void dfs2(int i, int n, int p = -1) {
+        for(auto u : adj[i]) {
+            if(u == p) continue;
+            res[u] = res[i] - counter[u] + n - counter[u];
+            dfs2(u, n, i);
+        }
+    }
+    
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        adj.resize(n);
+        //Adjacency List
+        for(int i = 0; i < n - 1; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        res.resize(n);
+        counter.resize(n);
+        dfs(0);
+        dfs2(0, n);
+        return res;
+    }
+};
