4cb266a13d0d66c3b73df4e371cec8de50daefdd
Harsh Santoshi <115166906+HarshSantoshi@users.noreply.github.com>
2022-12-26 18:48:42 +0530

    Create 55JumpGame.cpp

---
+++ 55JumpGame.cpp
@@ -0,0 +1,9 @@
+//T.C=O(N)
+
+bool canJump(vector<int>& nums) {
+        int n= nums.size();
+    int i = 0;
+    for (int reach = 0; i < n && i <= reach; ++i)
+        reach = max(i + nums[i], reach);
+    return i == n;
+    }
