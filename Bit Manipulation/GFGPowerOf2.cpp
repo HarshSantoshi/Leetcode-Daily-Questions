441a21528d9ab3d53c2d43d3b634ef953de2f828
Harsh Santoshi <115166906+HarshSantoshi@users.noreply.github.com>
2023-01-08 17:55:21 +0530

    Create GFGPowerOf2.cpp

---
+++ Bit Manipulation/GFGPowerOf2.cpp
@@ -0,0 +1,24 @@
+/*
+Given a non-negative integer N. The task is to check if N is a power of 2. More formally, check if N can be expressed as 2x for some x.
+*/
+
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here    
+        if( n==0){
+            return false;
+        }
+        while(n>1){
+            if(n&1){
+               return false;
+            }
+            n=n>>1;
+        }
+        return true;
+        
+    }
+};
