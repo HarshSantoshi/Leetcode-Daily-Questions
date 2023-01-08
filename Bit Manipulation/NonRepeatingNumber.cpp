3241e543b24e07189a294a1f95756667a44701ea
Harsh Santoshi <115166906+HarshSantoshi@users.noreply.github.com>
2023-01-08 17:44:41 +0530

    Update NonRepeatingNumber.cpp

--- Bit Manipulation/NonRepeatingNumber.cpp
+++ Bit Manipulation/NonRepeatingNumber.cpp
@@ -1,3 +1,8 @@
+
+/*
+Given an array A containing 2*N+2 positive numbers, out of which 2*N numbers exist in pairs
+whereas the other two number occur exactly once and are distinct. Find the other two numbers. Return in increasing order.
+*/
 class Solution
 {
 public:
